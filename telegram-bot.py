7521d770b6f47e9a524e69c233e2785f4bd6a698
clariss <clarisstorm52@gmail.com>
2025-03-21 17:28:30 +0100

    Fix deployment configuration

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from telegram import Update, ReactionTypeEmoji
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackContext, MessageHandler, filters
 
@@ -7,12 +8,14 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+TOKEN = "7652627861:AAHhq0HUA-15Ig71m-HY7gMp2zhzRkr4wxg"
+WEBHOOK_URL = "https://ton-app-render.onrender.com/webhook"  # Remplace par l'URL de ton app Render
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="This is a reaction bot.")
 
 async def reaction(update: Update, context: CallbackContext):
     emoji = "😱"
-    # Gère à la fois les messages normaux et les posts de canal
     chat_id = update.channel_post.chat_id if update.channel_post else update.message.chat_id
     message_id = update.channel_post.message_id if update.channel_post else update.message.message_id
     
@@ -22,16 +25,22 @@ async def reaction(update: Update, context: CallbackContext):
         logging.error(f"Erreur lors de l'ajout de la réaction: {e}")
 
 print("Bot Is Working")
-app = ApplicationBuilder().token("7652627861:AAHhq0HUA-15Ig71m-HY7gMp2zhzRkr4wxg").build()
+app = ApplicationBuilder().token(TOKEN).build()
 
 start_handler = CommandHandler('start', start)
 app.add_handler(start_handler)
 
-# Handler pour les messages textuels dans les groupes, messages privés ET canaux
 react_handler = MessageHandler(
     (filters.TEXT | filters.ChatType.CHANNEL) & ~filters.COMMAND, 
     reaction
 )
 app.add_handler(react_handler)
 
-app.run_polling()
+# Configuration du Webhook au lieu du Polling
+PORT = int(os.environ.get("PORT", 8443))
+app.run_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    url_path="webhook",
+    webhook_url=WEBHOOK_URL
+)
